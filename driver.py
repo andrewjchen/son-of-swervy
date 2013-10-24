892f7dc7bc2cca34b854c33e0d7af757e825bce3
Andrew J. Chen <andrew.chen@berkeley.edu>
2013-10-24 16:02:04 -0700

    added pololu protocol

--- driver.py
+++ driver.py
@@ -13,11 +13,23 @@ def driveChannel(channel, command):
     serial.write(chr(channel))
     serial.write(chr(command))
 
+def pololuDrive(device, channel, target):
+    '''
+    Drives a servo on a maestro using the pololu protocol.
+    DEVICE is the maestro device number. (default 12)
+    CHANNEL is the servo's channel number [1...6] on micromaestro
+    TARGET is the pulse width in microseconds.
+    '''
+    global serial
+    serial.write(chr(0xAA)) # 0xAA
+    serial.write(chr(device))
+    serial.write(chr(4))
+    serial.write(chr(channel))
+    serial.write(chr((target*4) & 0x7F))
+    serial.write(chr(((target*4) >> 7) & 0x7F))
+
 if __name__ == '__main__':
     global serial
     print "starting driver.py..."
     serial = serial.Serial("/dev/ttyUSB0", baudrate=57600)
-    driveChannel(0, 0);
-    driveChannel(1, 0);
-    driveChannel(2, 0);
-
+    pololuDrive(12, 0, 1500);
